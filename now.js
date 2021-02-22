26dc23465197632f88957f724f01110c861945f4
Carlos Hinostroza <chinostroza@gmail.com>
2021-02-22 19:11:36 +0000

    init repo

---
+++ now.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var swig = require('swig');
+var path = require('path');
+var app = express();
+const bodyParser = require("body-parser")
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://10.136.103.77:27017/";
+
+var routes = require('./routes/index');
+
+app.engine('html',swig.renderFile);
+
+app.set('view engine','html');
+app.set('views',path.join(__dirname, 'views'));
+app.set('view cache',false);
+swig.setDefaults({ cache: false });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/webhook', (request, response) => {
+  const { body } = request;
+  console.log(body);
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("now");
+ 
+    dbo.collection("account").insertOne(body, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+});
+
+var server = app.listen(6000, function () {
+   var host = server.address().address
+   console.log(host)
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ public/prototipo.js
@@ -0,0 +1,17 @@
+
+
+window.onload=function(){
+ 
+    var buttonfake = document.getElementById("buttonfake"); 
+  
+    buttonfake.addEventListener("click", handler);
+
+}
+
+
+function handler() {
+    var lighbox = document.getElementById("lightbox")
+    lighbox.style.display = "block";
+    
+    
+}
